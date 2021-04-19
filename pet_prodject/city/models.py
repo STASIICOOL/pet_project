9da4a81089b482c3809cb7b23b8a796610e573cc
line_four <stas22.08.2004@gmail.com>
2021-04-19 20:07:14 +0200

    createapp model_city

--- pet_prodject/city/models.py
+++ pet_prodject/city/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=150)
+    flag = models.ImageField(blank=True, null=True, upload_to="flags/")
+    emblem = models.ImageField(blank=True, null=True, upload_to="emblems/")
+    description = models.CharField()
+    residents = models.IntegerField(default=0)
+    longitude = models.IntegerField(default=0)
+    latitude = models.IntegerField(default=0)
