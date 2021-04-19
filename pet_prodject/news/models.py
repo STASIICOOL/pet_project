5eaa88f4d4427c5cdfc3156abb00b842b138baf7
line_four <stas22.08.2004@gmail.com>
2021-04-19 20:20:24 +0200

    create model_news

--- pet_prodject/news/models.py
+++ pet_prodject/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from city.models import City
 # Create your models here.
+
+class News(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField()
+    image = models.ImageField()
+    city = models.ForeignKey(City,on_delete=models.DO_NOTHING)
