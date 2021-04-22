6a1d74fcb4aa35d51016e24d8df500beb08a4a12
line_four <stas22.08.2004@gmail.com>
2021-04-22 19:55:59 +0200

    create model_food_type

--- pet_prodject/food_point/models.py
+++ pet_prodject/food_point/models.py
@@ -11,3 +11,10 @@ class FoodPlace(models.Model):
     item_type = models.CharField(max_length=150)
     working_hours = models.CharField(max_length=150)
     city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
+
+class Food(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    price = models.CharField(max_length=50)
+    image = models.ImageField()
+    food_place = models.ForeignKey(FoodPlace, on_delete=models.DO_NOTHING)
