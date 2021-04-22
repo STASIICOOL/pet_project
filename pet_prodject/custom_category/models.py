f46133f6f1219f68191a3d19ee1afab0b6b4dae5
line_four <stas22.08.2004@gmail.com>
2021-04-22 20:38:16 +0200

    create model_category

--- pet_prodject/custom_category/models.py
+++ pet_prodject/custom_category/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from city.models import City
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
+
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+    description = models.CharField(200)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
