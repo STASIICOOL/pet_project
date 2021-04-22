c2245c5e2bac4e442057c6f0b4328d672b41f0fb
line_four <stas22.08.2004@gmail.com>
2021-04-22 20:13:25 +0200

    create model_housing

--- pet_prodject/booking/models.py
+++ pet_prodject/booking/models.py
@@ -11,3 +11,10 @@ class Housing(models.Model):
     checkin_time = models.CharField(max_length=150)
     checkout_time = models.CharField(max_length=150)
     city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
+
+class Number_housing(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    image = models.ImageField()
+    price = models.CharField()
+    city = models.ForeignKey(Housing, on_delete=models.DO_NOTHING)
