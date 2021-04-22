3edc671b425e6f528c0f39823b668671470f9488
line_four <stas22.08.2004@gmail.com>
2021-04-22 21:10:09 +0200

    create registration_models

--- pet_prodject/food_point/admin.py
+++ pet_prodject/food_point/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import FoodPlace, Food
 # Register your models here.
+
+admin.site.register(FoodPlace)
+admin.site.register(Food)
--- pet_prodject/interest_place/admin.py
+++ pet_prodject/interest_place/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import InterestingPlace
 # Register your models here.
+
+admin.site.register(InterestingPlace)
