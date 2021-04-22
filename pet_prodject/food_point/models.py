e85374f6ca336debb01031ae28052df4edb0ec16
line_four <stas22.08.2004@gmail.com>
2021-04-22 19:28:45 +0200

    create model_food_place

--- pet_prodject/food_point/models.py
+++ pet_prodject/food_point/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from city.models import City
 
 # Create your models here.
 
 class FoodPlace(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField()
-    longitude =
-    latitude =
+    description = models.CharField(max_length=150)
+    longitude = models.IntegerField()
+    latitude = models.IntegerField()
+    item_type = models.CharField(max_length=150)
+    working_hours = models.CharField(max_length=150)
+    city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
