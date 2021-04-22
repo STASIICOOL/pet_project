8d15e347ab881966f2627e8d69015de4d2f06fb6
line_four <stas22.08.2004@gmail.com>
2021-04-22 20:08:48 +0200

    create model_housing

--- pet_prodject/booking/models.py
+++ pet_prodject/booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from city.models import City
 # Create your models here.
+
+class Housing(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    image = models.ImageField()
+    longitude = models.IntegerField()
+    latitude = models.IntegerField()
+    checkin_time = models.CharField(max_length=150)
+    checkout_time = models.CharField(max_length=150)
+    city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
