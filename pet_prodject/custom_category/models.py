13313a9df9947028debb59affcbd0c8c7b404cb5
line_four <stas22.08.2004@gmail.com>
2021-04-22 21:01:22 +0200

    create migrations

--- pet_prodject/custom_category/models.py
+++ pet_prodject/custom_category/models.py
@@ -9,6 +9,6 @@ class Category(models.Model):
 class Post(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField()
-    description = models.CharField(200)
+    description = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     city = models.ForeignKey(City, on_delete=models.DO_NOTHING)
